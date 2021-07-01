669572e9455e7d516a64df05e5289b51e20e6908
sevenc-nanashi <sevenc-nanashi@sevenbot.jp>
2021-07-02 06:13:28 +0900

    Update: Update version

--- discord/ext/components/__init__.py
+++ discord/ext/components/__init__.py
@@ -2,7 +2,7 @@ from .sender import send, edit, reply, Button, ButtonType, SelectMenu, SelectOpt
 from .receiver import ComponentCog  # noqa
 from .responder import ButtonResponse, SelectMenuResponse  # noqa
 
-__version__ = "0.3.3"
+__version__ = "0.3.4"
 
 
 def setup(bot):
